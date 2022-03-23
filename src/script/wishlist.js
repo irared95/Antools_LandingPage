ad25b7e88e9824c7658f32f2f812c04063f64ed9
Ira Redchets <irared95@gmail.com>
2022-03-23 21:46:05 +0200

    finished tools and newcomer-tools

--- src/script/wishlist.js
+++ src/script/wishlist.js
@@ -1,3 +1,12 @@
 (function () {
-    
+    let links = document.querySelectorAll('.add-wishlist--js')
+
+    links.forEach(function(link, index) {
+        link.addEventListener('click', function (e){
+            e.preventDefault()
+            link.classList.toggle('active');
+            console.log(e)
+        })
+    });
+    console.log(links)
 })();
