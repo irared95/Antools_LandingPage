e8f9977378528e914ec14bf458d94d4068934aca
Ira Redchets <irared95@gmail.com>
2022-03-30 00:40:06 +0300

    finish adaptive, burger

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // $('.burger--js').on('click', function (){
+    //     if($('.burger--js').hasClass('active')) {
+    //         $('.nav--js').removeClass('active');
+    //     } else {
+    //         $('.nav--js').addClass('active');
+    //     }
+    // })
 
+    const burger = document.querySelector('.burger--js');
+    const navMenu = document.querySelector('.nav--js');
+
+    burger.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+        this.classList.toggle('active');
+    })
+
+});
 
 /*!
  * jQuery JavaScript Library v3.6.0
